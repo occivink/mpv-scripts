ac05802882387670370d2bef25a97f33528cd956
Olivier Perret <Olivier.Perret@mailbox.org>
2017-12-03 19:04:31 +0100

    blur-edges: fix blur parameters when zone is too small

--- blur-edges.lua
+++ blur-edges.lua
@@ -36,24 +36,28 @@ function set_blur()
     local par = mp.get_property_number("video-params/par")
     local height = mp.get_property_number("video-params/h")
     local width = mp.get_property_number("video-params/w")
-    local blur = string.format("boxblur=lr=%i:lp=%i", opts.blur_radius, opts.blur_power)
     
     local split = "[vid1] split=3 [a] [v] [b]"
-
     local crop_format = "crop=%s:%s:%s:%s"
 
-    local stack_direction, crop_1, crop_2
+    local stack_direction, crop_1, crop_2, blur_size
     if  ww/wh > video_aspect then
-        local blur_width = math.floor(((ww/wh)*height/par-width)/2)
-        crop_1 = string.format(crop_format, blur_width, height, "0", "0")
-        crop_2 = string.format(crop_format, blur_width, height, width - blur_width, "0")
-        stack_direction = "hstack"
+        blur_size = math.floor(((ww/wh)*height/par-width)/2)
+        crop_1 = string.format(crop_format, blur_size, height, "0", "0")
+        crop_2 = string.format(crop_format, blur_size, height, width - blur_size, "0")
+        stack_direction = "h"
     else
-        local blur_height = math.floor(((wh/ww)*width*par-height)/2)
-        crop_1 = string.format(crop_format, width, blur_height, "0", "0")
-        crop_2 = string.format(crop_format, width, blur_height, "0", height - blur_height)
-        stack_direction = "vstack"
+        blur_size = math.floor(((wh/ww)*width*par-height)/2)
+        crop_1 = string.format(crop_format, width, blur_size, "0", "0")
+        crop_2 = string.format(crop_format, width, blur_size, "0", height - blur_size)
+        stack_direction = "v"
     end
+    if blur_size < 2 then return end
+    local lr = math.min(opts.blur_radius, math.floor(blur_size/2)-1)
+    local cr = math.min(opts.blur_radius, math.floor(blur_size/4)-1)
+    local blur = string.format("boxblur=lr=%i:lp=%i:cr=%i:cp=%i",
+        lr, opts.blur_power, cr, opts.blur_power)
+
     zone_1 = string.format("[a] %s,%s [a_fin]", crop_1, blur)
     zone_2 = string.format("[b] %s,%s [b_fin]", crop_2, blur)
 
@@ -62,9 +66,10 @@ function set_blur()
        par_fix = ",setsar=ratio=" .. tostring(par) .. ":max=10000"
     end
 
-    stack = string.format("[a_fin] [v] [b_fin] %s=3%s [vo]", stack_direction, par_fix)
+    stack = string.format("[a_fin] [v] [b_fin] %sstack=3%s [vo]", stack_direction, par_fix)
     filter = string.format("%s;%s;%s;%s", split, zone_1, zone_2, stack)
     set_lavfi_complex(filter)
+    applied = true
 end
 
 function unset_blur()
