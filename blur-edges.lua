d080d62ca196d41b3fdc6090e8d8ed1eddf54715
Olivier Perret <Olivier.Perret@mailbox.org>
2017-12-03 12:42:56 +0100

    blur-edges: also work for top and bottom black bars

--- blur-edges.lua
+++ blur-edges.lua
@@ -4,6 +4,7 @@ local opts = {
     blur_radius = 10,
     blur_power = 10,
     auto_apply = true,
+    mode = "all",
     reapply_delay = 0.5,
 }
 options.read_options(opts)
@@ -24,48 +25,51 @@ function set_lavfi_complex(filter)
 end
 
 function set_blur()
-    if applied then return end
     if not mp.get_property("video-out-params") then return end
     local video_aspect = mp.get_property_number("video-aspect")
     local ww, wh = mp.get_osd_size()
-    if ww/wh < video_aspect + 0.01 then return end
 
-    local split = "[vid1] split=3 [left] [v] [right]"
-    local blur = string.format("boxblur=lr=%i:lp=%i", opts.blur_radius, opts.blur_power)
+    if math.abs(ww/wh - video_aspect) < 0.05 then return end
+    if opts.mode == "horizontal" and ww/wh < video_aspect then return end
+    if opts.mode == "vertical" and ww/wh > video_aspect then return end
 
     local par = mp.get_property_number("video-params/par")
     local height = mp.get_property_number("video-params/h")
     local width = mp.get_property_number("video-params/w")
+    local blur = string.format("boxblur=lr=%i:lp=%i", opts.blur_radius, opts.blur_power)
     
-    local blur_width = math.floor(((ww/wh)*height/par-width)/2)
-    local crop_format = "crop=%s:%s:%s:0"
-    local crop_left = string.format(crop_format, blur_width, height, "0")
-    local crop_right = string.format(crop_format, blur_width, height, width-blur_width)
+    local split = "[vid1] split=3 [a] [v] [b]"
 
-    local left = string.format("[left] %s,%s [left_fin]", crop_left, blur)
-    local right = string.format("[right] %s,%s [right_fin]", crop_right, blur)
+    local crop_format = "crop=%s:%s:%s:%s"
+
+    local stack_direction, crop_1, crop_2
+    if  ww/wh > video_aspect then
+        local blur_width = math.floor(((ww/wh)*height/par-width)/2)
+        crop_1 = string.format(crop_format, blur_width, height, "0", "0")
+        crop_2 = string.format(crop_format, blur_width, height, width - blur_width, "0")
+        stack_direction = "hstack"
+    else
+        local blur_height = math.floor(((wh/ww)*width*par-height)/2)
+        crop_1 = string.format(crop_format, width, blur_height, "0", "0")
+        crop_2 = string.format(crop_format, width, blur_height, "0", height - blur_height)
+        stack_direction = "vstack"
+    end
+    zone_1 = string.format("[a] %s,%s [a_fin]", crop_1, blur)
+    zone_2 = string.format("[b] %s,%s [b_fin]", crop_2, blur)
 
     local par_fix = ""
     if par ~= 1 then
-       par_fix = ",setsar=r=" .. tonumber(par)
+       par_fix = ",setsar=ratio=" .. tostring(par) .. ":max=10000"
     end
-    stack = string.format("[left_fin] [v] [right_fin] hstack=3%s [vo]", par_fix)
-    set_lavfi_complex(string.format("%s;%s;%s;%s", split, left, right, stack))
-    applied = true
+
+    stack = string.format("[a_fin] [v] [b_fin] %s=3%s [vo]", stack_direction, par_fix)
+    filter = string.format("%s;%s;%s;%s", split, zone_1, zone_2, stack)
+    set_lavfi_complex(filter)
 end
 
 function unset_blur()
-    if not applied then return end
     set_lavfi_complex()
     applied = false
-end
-
-function toggle_blur()
-    if applied then
-        unset_blur()
-    else
-        set_blur()
-    end
 end
 
 local timer = nil
