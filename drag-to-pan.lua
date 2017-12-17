5c8099500866f3d8b34da6225b9c34cd5c32c4b8
Olivier Perret <Olivier.Perret@mailbox.org>
2017-12-17 12:43:58 +0100

    drag-to-pan: do not change pan when the entire picture is visible

--- drag-to-pan.lua
+++ drag-to-pan.lua
@@ -5,6 +5,7 @@ local mouse_pos_origin = {}
 
 local opts = {
     margin = 50,
+    do_not_move_if_all_visible = true,
 }
 local options = require 'mp.options'
 options.read_options(opts)
@@ -116,8 +117,16 @@ function pan_follows_cursor_idle()
         local window_w, window_h = mp.get_osd_size()
         local x = math.min(1, math.max(- 2 * mX / window_w + 1, -1))
         local y = math.min(1, math.max(- 2 * mY / window_h + 1, -1))
-        mp.set_property_number("video-pan-x", x * (video_dimensions.w - window_w + 2 * opts.margin) / (2 * (video_dimensions.w)))
-        mp.set_property_number("video-pan-y", y * (video_dimensions.h - window_h + 2 * opts.margin) / (2 * (video_dimensions.h)))
+        if (opts.do_not_move_if_all_visible and window_w < video_dimensions.w) then
+            mp.set_property_number("video-pan-x", x * (video_dimensions.w - window_w + 2 * opts.margin) / (2 * (video_dimensions.w)))
+        elseif mp.get_property_number("video-pan-x") then
+            mp.get_property_number("video-pan-x", 0)
+        end
+        if (opts.do_not_move_if_all_visible and window_h < video_dimensions.h) then
+            mp.set_property_number("video-pan-y", y * (video_dimensions.h - window_h + 2 * opts.margin) / (2 * (video_dimensions.h)))
+        elseif mp.get_property_number("video-pan-x") then
+            mp.get_property_number("video-pan-x", 0)
+        end
         needs_adjusting = false
     end
 end
@@ -136,5 +145,5 @@ function pan_follows_cursor_handler(table)
     end
 end
 
-mp.add_key_binding("MOUSE_BTN0", "drag-to-pan", drag_to_pan_handler, {complex = true})
+mp.add_key_binding(nil, "drag-to-pan", drag_to_pan_handler, {complex = true})
 mp.add_key_binding(nil, "pan-follows-cursor", pan_follows_cursor_handler, {complex = true})
