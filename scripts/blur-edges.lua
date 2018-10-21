32ed1f43591a79a2e6c20ceb833a769bb731b85b
Olivier Perret <Olivier.Perret@mailbox.org>
2018-10-21 23:07:41 +0200

    Change how blurring works; the blurred 'background' now shows the video scaled by the other direction
    Fixes #24

--- scripts/blur-edges.lua
+++ scripts/blur-edges.lua
@@ -48,37 +48,61 @@ function set_blur()
     local par = mp.get_property_number("video-params/par")
     local height = mp.get_property_number("video-params/h")
     local width = mp.get_property_number("video-params/w")
-    
+
     local split = "[vid1] split=3 [a] [v] [b]"
     local crop_format = "crop=%s:%s:%s:%s"
+    local scale_format = "scale=width=%s:height=%s:flags=neighbor"
 
-    local stack_direction, crop_1, crop_2, blur_size
-    if  ww/wh > video_aspect then
-        blur_size = math.min(math.floor(((ww/wh)*height/par-width)/2), width/2)
-        crop_1 = string.format(crop_format, blur_size, height, "0", "0")
-        crop_2 = string.format(crop_format, blur_size, height, width - blur_size, "0")
+    local stack_direction, cropped_scaled_1, cropped_scaled_2, blur_size
+
+    if ww/wh > video_aspect then
+        blur_size = math.floor((ww/wh)*height/par-width)
+        local nudge = blur_size % 2
+        blur_size = blur_size / 2
+
+        local height_with_maximized_width = height / width * ww
+        local visible_height = math.floor(height * wh / height_with_maximized_width)
+        local visible_width = math.floor(blur_size * wh / height_with_maximized_width)
+
+        local cropped_1 = string.format(crop_format, visible_width, visible_height, "0", (height - visible_height)/2)
+        local scaled_1 = string.format(scale_format, blur_size + nudge, height)
+        cropped_scaled_1 = cropped_1 .. "," .. scaled_1
+
+        local cropped_2 = string.format(crop_format, visible_width, visible_height, width - visible_width, (height - visible_height)/2)
+        local scaled_2 = string.format(scale_format, blur_size , height)
+        cropped_scaled_2 = cropped_2 .. "," .. scaled_2
         stack_direction = "h"
     else
-        blur_size = math.min(math.floor(((wh/ww)*width*par-height)/2), height/2)
-        crop_1 = string.format(crop_format, width, blur_size, "0", "0")
-        crop_2 = string.format(crop_format, width, blur_size, "0", height - blur_size)
+        blur_size = math.floor((wh/ww)*width*par-height)
+        local nudge = blur_size % 2
+        blur_size = blur_size / 2
+
+        local width_with_maximized_height = width / height * wh
+        local visible_width = math.floor(width * ww / width_with_maximized_height)
+        local visible_height = math.floor(blur_size * ww / width_with_maximized_height)
+
+        local cropped_1 = string.format(crop_format, visible_width, visible_height, (width - visible_width)/2, "0")
+        local scaled_1 = string.format(scale_format, width, blur_size + nudge)
+        cropped_scaled_1 = cropped_1 .. "," .. scaled_1
+
+        local cropped_2 = string.format(crop_format, visible_width, visible_height, (width - visible_width)/2, height - visible_height)
+        local scaled_2 = string.format(scale_format, width, blur_size)
+        cropped_scaled_2 = cropped_2 .. "," .. scaled_2
         stack_direction = "v"
     end
+
     if blur_size < math.max(1, opts.minimum_black_bar_size) then return end
     local lr = math.min(opts.blur_radius, math.floor(blur_size/2)-1)
     local cr = math.min(opts.blur_radius, math.floor(blur_size/4)-1)
     local blur = string.format("boxblur=lr=%i:lp=%i:cr=%i:cp=%i",
         lr, opts.blur_power, cr, opts.blur_power)
 
-    zone_1 = string.format("[a] %s,%s [a_fin]", crop_1, blur)
-    zone_2 = string.format("[b] %s,%s [b_fin]", crop_2, blur)
+    zone_1 = string.format("[a] %s,%s [a_fin]", cropped_scaled_1, blur)
+    zone_2 = string.format("[b] %s,%s [b_fin]", cropped_scaled_2, blur)
 
-    local par_fix = ""
-    if par ~= 1 then
-       par_fix = ",setsar=ratio=" .. tostring(par) .. ":max=10000"
-    end
+    local par_fix = "setsar=ratio=" .. tostring(par) .. ":max=10000"
 
-    stack = string.format("[a_fin] [v] [b_fin] %sstack=3%s [vo]", stack_direction, par_fix)
+    stack = string.format("[a_fin] [v] [b_fin] %sstack=3,%s [vo]", stack_direction, par_fix)
     filter = string.format("%s;%s;%s;%s", split, zone_1, zone_2, stack)
     set_lavfi_complex(filter)
     applied = true
